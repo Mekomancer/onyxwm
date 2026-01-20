4ecf29f6b61a5c56df53cffa27b9dfe83597859c
Toby Hansen <tobyhansen@outlook.com>
2026-01-19 16:44:58 -0800

    fix spawnAct, add keybindings

--- src/config/autostart.h
+++ src/config/autostart.h
@@ -2,7 +2,6 @@
 // List arguments and then end with a null
 // increase the number if arguments go over 4
 static char *autostart_list[][4] = {
-	{"st", NULL},
 	{"picom", NULL},
 	{"xwallpaper","--zoom", "/home/void/.config/wallpaper", NULL},
 	{"xbanish", NULL},
--- src/config/keybindings.h
+++ src/config/keybindings.h
@@ -5,8 +5,13 @@
 #define SHIFT XCB_MOD_MASK_SHIFT
 #define CTRL XCB_MOD_MASK_CONTROL
 
+static const char *launcher_cmd[] = {"j4-dmenu-desktop", NULL};
+static const char *term_cmd[] = {"st", NULL};
+
 // keybinding table, intended to modified
 // modifier key, key, function, argument
 static const keybinding_t keys[] = {
-	{MOD|SHIFT, XK_Escape,        quit,        {0}}, 
+	{MOD,        XK_r,         spawnAct,        {.v = launcher_cmd}}, 
+	{MOD,        XK_Return,             spawnAct,    {.v = term_cmd}}, 
+	{MOD|SHIFT,  XK_Escape,        quit,        {0}}, 
 };
